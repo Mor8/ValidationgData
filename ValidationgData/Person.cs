8c5b70abc6d51ee4426142d6f7c3c96e012cde57
Mor8 <fukuya.mori@zeiss.com>
2018-01-21 13:01:59 +0900

    Personクラスの追加

---
+++ ValidationgData/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationgData
+{
+    class Person : INotifyPropertyChanged
+    {
+        protected virtual void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        string _name;
+        public string Name
+        {
+            get { return _name;}
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
